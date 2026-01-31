a9888af72be3b9b2133d4ca52bdb329f4162de90
Uday <udayashokpatel@gmail.com>
2026-01-31 22:31:55 +0530

    commit

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -57,7 +57,7 @@ exports.loginUser=async (req,res)=>{
 }
 exports.getUserInfo=async (req,res)=>{
     try{
-        const user=await User.findOne(req.user._id).select("-password")
+        const user=await User.findById(req.user._id).select("-password")
         if(!user){
             res.status(404).json({message:"User not found"})
         }
